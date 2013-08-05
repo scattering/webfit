e7c2c8524f7c07e533f13ccdd3048d52996b09ea
Erica Peng <erica@P628864.(none)>
2013-08-05 08:24:15 -0400

    Debugging Simplex

--- simplex.js
+++ simplex.js
@@ -21,6 +21,9 @@ function simplex(f, x0, bounds, radius, xtol, ftol, maxiter, update_handler, abo
     if (typeof update_handler === 'undefined') {
         var update_handler = null;
     }
+    var dont_abort = function(){
+        return false;
+    }
     if (typeof abort_test === 'undefined') {
         var abort_test = dont_abort;
     }
@@ -31,11 +34,12 @@ function simplex(f, x0, bounds, radius, xtol, ftol, maxiter, update_handler, abo
         var toReturn = [];
         for(var row = 0; row < array.length; row++){
             for(var col = 0; col < array[0].length; col++){
-                toReturn.push(array[row][col];
+                toReturn.push(array[row][col]);
             }
         }
+        return toReturn;
     }
-    x0 = this.flatten(x0);
+    x0 = flatten(x0);
     var radius = 0.05;
     var val = x0*(1+radius);
     var N = x0.length;
@@ -49,24 +53,33 @@ function simplex(f, x0, bounds, radius, xtol, ftol, maxiter, update_handler, abo
             for(var length = 1; length < lengthArray[0]; length++){
                 toReturn.push(row);
             }
-            return toReturn;
         }
+        else{
+            var toReturn = [];
+            for(var i = 0; i < lengthArray[0]; i++){
+                toReturn.push(num);
+            }
+        }
+        
+        return toReturn;
     }
-    var sim = this.numArray(0, [N+1,N]/*, dtype=x0.dtype*/);
-    var fsim = this.numArray(0, [N+1]/*, float*/);
+    var sim = numArray(0, [N+1,N]/*, dtype=x0.dtype*/);
+    var fsim = numArray(0, [N+1]/*, float*/);
     sim[0] = x0;
     var wrap_function = function(fun, bounds){
         var ncalls = [];
         var function_wrapper = undefined;
-        if(bounds !== undefined){
+        if(bounds !== null){
             var lo = bounds[0];
             var hi = bounds[1];
             function_wrapper = function(x){
                 ncalls[0] += 1;
                 if(x < lo || x > hi){
                     return Infinity;
+                }
                 else
                     return fun(x);
+            }
         }
         else{
             function_wrapper = function(x){
@@ -80,8 +93,9 @@ function simplex(f, x0, bounds, radius, xtol, ftol, maxiter, update_handler, abo
     fsim[0] = func(x0);
     var maxiter = N * 200;
     var xtol = Math.pow(1, -4);
+    var dim2 = x0[0].length;
     var x0Shape = [x0.length, x0[0].length];
-    var tol = this.numArray(1, x0Shape)*xtol;//***
+    var tol = numArray(1, x0Shape)*xtol;//***
     var bounded = lo === Infinity && hi === Infinity;
     tol[bounded] = (hi[bounded]-lo[bounded])*xtol;
     var xtol = tol;
@@ -106,8 +120,8 @@ function simplex(f, x0, bounds, radius, xtol, ftol, maxiter, update_handler, abo
     }
 
     ind = fsim.sort();
-    fsim = this.take(fsim, ind, 0);
-    sim = this.take(sim, ind, 0);
+    fsim = take(fsim, ind, 0);
+    sim = take(sim, ind, 0);
 
     var sum = function(array, axis){
         var toReturn = 0;
@@ -128,11 +142,19 @@ function simplex(f, x0, bounds, radius, xtol, ftol, maxiter, update_handler, abo
 
     var iterations = 1;
     while(iteractions < maxiter){
-        if(numpy.all(Math.abs(sim.slice(1,sim.length)-sim[0])<=xtol) && Math.max(Math.abs(fsim[0] - fsim.slice(1,sim.length)) <= ftol){
+        var toAnalyze = sim.slice(1,sim.length);
+        var allTrue = true;
+        for(var i = 0; i < toAnalyze.length; i++){
+            if(!(Math.abs(toAnalyze[i] - sim[0]) <= xtol)){
+                allTrue = false;
+                i = toAnalyze.length;
+            }
+        }
+        if(allTrue && Math.max(Math.abs(fsim[0] - fsim.slice(1,sim.length)) <= ftol)){
             break;
         }
         
-        var xbar = this.sum(sim.slice(0,sim.length-1),0) / N; 
+        var xbar = sum(sim.slice(0,sim.length-1),0) / N; 
         var xr = (1+rho)*xbar - rho*sim[sim.length-1];
         var fxr = func(xr);
         var doshrink = 0;
@@ -169,8 +191,8 @@ function simplex(f, x0, bounds, radius, xtol, ftol, maxiter, update_handler, abo
             }
         }
         var ind = fsim.sort();
-        sim = this.take(sim,ind,0);
-        fsim = this.take(fsim,ind,0);
+        sim = take(sim,ind,0);
+        fsim = take(fsim,ind,0);
         if(update_handler != undefined)
             update_handler(iterations, maxiter, sim, fsim);
         iterations += 1;
@@ -178,3 +200,72 @@ function simplex(f, x0, bounds, radius, xtol, ftol, maxiter, update_handler, abo
             break;
     }
 }
+
+var rosen = function(x){
+    return x*x;
+}
+
+var x0 = [0.8,1.2,0.7];
+console.log("Nelder-Mead Simplex");
+console.log("===================");
+var start = new Date().getTime() / 1000;
+var x = simplex(rosen,x0);
+console.log(x);
+console.log("Time:",new Date().getTime() / 1000 - start);
+
+x0 = [0,0,0];
+console.log("Nelder-Mead Simplex");
+console.log("===================");
+console.log("starting at zero");
+start = new Date().getTime() / 1000;
+x = simplex(rosen,x0);
+console.log(x);
+console.log("Time:",new Date().getTime() / 1000 - start);
+
+x0 = [0.8,1.2,0.7];
+var lo = [0,0,0];
+var hi = [1,1,1];
+console.log("Bounded Nelder-Mead Simplex");
+console.log("===========================");
+start = new Date().getTime() / 1000;
+x = simplex(rosen,x0,[lo,hi]);
+console.log(x);
+console.log("Time:",new Date().getTime() / 1000 - start);
+
+x0 = [0.8,1.2,0.7];
+lo = [0.999,0.999,0.999];
+hi = [1.001,1.001,1.001];
+console.log("Bounded Nelder-Mead Simplex");
+console.log("===========================");
+console.log("tight bounds");
+console.log("simplex is smaller than 1e-7 in every dimension, but you can't");
+console.log("see this without uncommenting the print statement simplex function");
+start = new Date().getTime() / 1000;
+x = simplex(rosen,x0,[lo,hi],xtol=1e-4);
+console.log(x);
+console.log("Time:",new Date().getTime() / 1000 - start);
+
+x0 = [0,0,0];
+lo = [0.999,0.999,0.999];
+hi = [1.001,1.001,1.001];
+console.log("Bounded Nelder-Mead Simplex");
+console.log("===========================");
+console.log("tight bounds, x0=0 outside bounds from above");
+start = new Date().getTime() / 1000;
+var rosenN = function(x){
+    return rosen(-x);
+}
+x = simplex(rosenN,x0,[lo,hi],xtol=1e-4);
+console.log(x);
+console.log("Time:",new Date().getTime() / 1000 - start);
+
+x0 = [0.8,1.2,0.7];
+lo = [-Infinity,-Infinity,-Infinity];
+hi = [Infinity,Infinity,Infinity];
+console.log("Bounded Nelder-Mead Simplex");
+console.log("===========================");
+console.log("infinite bounds");
+start = new Date().getTime() / 1000;
+x = simplex(rosen,x0,[lo,hi],xtol=1e-4);
+console.log(x);
+console.log("Time:",new Date().getTime() / 1000 - start);
