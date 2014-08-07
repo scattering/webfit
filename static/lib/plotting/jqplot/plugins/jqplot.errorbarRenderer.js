bc162c617bf4c56a4b268a9b59a4c15d10bb18c4
Alexander Zhang <alzhang96@gmail.com>
2014-08-07 16:09:49 -0400

    update errorbarrenderer

--- static/lib/plotting/jqplot/plugins/jqplot.errorbarRenderer.js
+++ static/lib/plotting/jqplot/plugins/jqplot.errorbarRenderer.js
@@ -85,9 +85,9 @@
         // true if is a hi-low-close chart (no open price).
         // This is determined automatically from the series data.
         this.hlc = false;
-        // prop: errorBar
-        // true to render error bars
-        // Must be specified manually.
+        // prop: showLine
+        // true to plot the line with LineRenderer on the series data
+        // defaults to true
         this.errorBar = false;
         // prop: lineWidth
         // Width of the hi-low line and open/close ticks.
@@ -109,180 +109,149 @@
         this.lineWidth = options.lineWidth || 1.5;
         $.jqplot.LineRenderer.prototype.init.call(this, options);
         this._type = 'errorbar';
-        // set the yaxis data bounds here to account for hi and low values
         var dbx = this._xaxis._dataBounds;
         var dby = this._yaxis._dataBounds;
         var d = this._plotData;
+        
         if (this.renderer.errorBar) {
-          // Loop through all points
-          for (var j=0; j<d.length; j++) {
-            // First set xupper, xlower, yupper, ylower
-            // Then adjust axis ranges if necessary
+          this._xaxis.resetDataBounds = newResetDataBounds;
+          this._yaxis.resetDataBounds = newResetDataBounds;
+        
+          this._xaxis.resetDataBounds(); 
+          this._yaxis.resetDataBounds();
           
-            var eb = d[j][2];
-            if (eb.xerr) {
-              // If symmetric
-              if (isNumeric(eb.xerr)) {
-                eb.xerr = [eb.xerr, eb.xerr];
-              }
-              if (jQuery.isArray(eb.xerr)) {
-                eb.xlower = d[j][0] - eb.xerr[0];
-                eb.xupper = d[j][0] + eb.xerr[1];
-              }
-            }
-            if (eb.yerr) {
-              // If symmetric
-              if (isNumeric(eb.yerr)) {
-                eb.yerr = [eb.yerr, eb.yerr];
-              }
-              if (jQuery.isArray(eb.yerr)) {
-                eb.ylower = d[j][1] - eb.yerr[0];
-                eb.yupper = d[j][1] + eb.yerr[1];
-              }
-            }
-            
-            if (eb.xlower < dbx.min || dbx.min == null) {
-                dbx.min = eb.xlower;
-            }
-            if (eb.xupper > dbx.max || dbx.max == null) {
-                dbx.max = eb.xupper;
-            }
-            if (eb.ylower < dby.min || dby.min == null) {
-                dby.min = eb.ylower;
-            }
-            if (eb.yupper > dby.max || dby.max == null) {
-                dby.max = eb.yupper;
-            }
-          }
-        }
+        } 
         
     };
     
     // called within scope of series.
     $.jqplot.errorbarRenderer.prototype.draw = function(ctx, gd, options) {
-        var d = this.data;
-        var xmin = this._xaxis.min;
-        var xmax = this._xaxis.max;
-        // index of last value below range of plot.
-        var xminidx = 0;
-        // index of first value above range of plot.
-        var xmaxidx = d.length;
-        var xp = this._xaxis.series_u2p;
-        var yp = this._yaxis.series_u2p;
-        var i, prevColor, ops, b, h, w, a, points;
-        var o;
-        var r = this.renderer;
-        var opts = (options != undefined) ? options : {};
-        var shadow = (opts.shadow != undefined) ? opts.shadow : this.shadow;
-        var fill = (opts.fill != undefined) ? opts.fill : this.fill;
-        var fillAndStroke = (opts.fillAndStroke != undefined) ? opts.fillAndStroke : this.fillAndStroke;
-        r.bodyWidth = (opts.bodyWidth != undefined) ? opts.bodyWidth : r.bodyWidth;
-        r.tickLength = (opts.tickLength != undefined) ? opts.tickLength : r.tickLength;
-        ctx.save();
-        if (this.show) {
-            var x, y, xu, xl, yu, yl;
-            // need to get widths based on number of points shown,
-            // not on total number of points.  Use the results 
-            // to speed up drawing in next step.
-            for (var i=0; i<d.length; i++) {
-                if (d[i][0] < xmin) {
-                    xminidx = i;
+        $.jqplot.LineRenderer.prototype.draw.call(this, ctx, gd, options);
+        if (this.renderer.errorBar == true) {
+            var d = this.data;
+            var xmin = this._xaxis.min;
+            var xmax = this._xaxis.max;
+            // index of last value below range of plot.
+            var xminidx = 0;
+            // index of first value above range of plot.
+            var xmaxidx = d.length;
+            var xp = this._xaxis.series_u2p;
+            var yp = this._yaxis.series_u2p;
+            var i, prevColor, ops, b, h, w, a, points;
+            var o;
+            var r = this.renderer;
+            var opts = (options != undefined) ? options : {};
+            var shadow = (opts.shadow != undefined) ? opts.shadow : this.shadow;
+            var fill = (opts.fill != undefined) ? opts.fill : this.fill;
+            var fillAndStroke = (opts.fillAndStroke != undefined) ? opts.fillAndStroke : this.fillAndStroke;
+            r.bodyWidth = (opts.bodyWidth != undefined) ? opts.bodyWidth : r.bodyWidth;
+            r.tickLength = (opts.tickLength != undefined) ? opts.tickLength : r.tickLength;
+            ctx.save();
+            if (this.show) {
+                var x, y, xu, xl, yu, yl;
+                // need to get widths based on number of points shown,
+                // not on total number of points.  Use the results 
+                // to speed up drawing in next step.
+                for (var i=0; i<d.length; i++) {
+                    if (d[i][0] < xmin) {
+                        xminidx = i;
+                    }
+                    else if (d[i][0] < xmax) {
+                        xmaxidx = i+1;
+                    }
                 }
-                else if (d[i][0] < xmax) {
-                    xmaxidx = i+1;
+
+                var dwidth = this.gridData[xmaxidx-1][0] - this.gridData[xminidx][0];
+                var nvisiblePoints = xmaxidx - xminidx;
+                try {
+                    var dinterval = Math.abs(this._xaxis.series_u2p(parseInt(this._xaxis._intervalStats[0].sortedIntervals[0].interval)) - this._xaxis.series_u2p(0)); 
                 }
-            }
 
-            var dwidth = this.gridData[xmaxidx-1][0] - this.gridData[xminidx][0];
-            var nvisiblePoints = xmaxidx - xminidx;
-            try {
-                var dinterval = Math.abs(this._xaxis.series_u2p(parseInt(this._xaxis._intervalStats[0].sortedIntervals[0].interval)) - this._xaxis.series_u2p(0)); 
-            }
-
-            catch (e) {
-                var dinterval = dwidth / nvisiblePoints;
-            }
-            
-            if (r.candleStick) {
-                if (typeof(r.bodyWidth) == 'number') {
-                    r._bodyWidth = r.bodyWidth;
+                catch (e) {
+                    var dinterval = dwidth / nvisiblePoints;
+                }
+                
+                if (r.candleStick) {
+                    if (typeof(r.bodyWidth) == 'number') {
+                        r._bodyWidth = r.bodyWidth;
+                    }
+                    else {
+                        r._bodyWidth = Math.min(20, dinterval/1.75);
+                    }
                 }
                 else {
-                    r._bodyWidth = Math.min(20, dinterval/1.75);
+                    if (typeof(r.tickLength) == 'number') {
+                        r._tickLength = r.tickLength;
+                    }
+                    else {
+                        r._tickLength = Math.min(10, dinterval/3.5);
+                    }
                 }
-            }
-            else {
-                if (typeof(r.tickLength) == 'number') {
-                    r._tickLength = r.tickLength;
-                }
-                else {
-                    r._tickLength = Math.min(10, dinterval/3.5);
+                
+                for (var i=xminidx; i<xmaxidx; i++) {
+                    x = xp(d[i][0]);
+                    y = yp(d[i][1]);
+
+                    xu = xp(d[i][2].xupper);
+                    xl = xp(d[i][2].xlower);
+                    yu = yp(d[i][2].yupper);
+                    yl = yp(d[i][2].ylower);
+                    close = yp(d[i][1]);
+                    //console.log('[',i,']: (x:',d[i][2].xlower,',',d[i][0],',',d[i][2].xupper,'; y:',d[i][2].ylower,',',d[i][1],',',d[i][2].yupper,')');
+
+                    o = {};
+                    
+                    prevColor = opts.color;
+                    if (r.openColor) {
+                        opts.color = r.openColor;
+                    }
+                    // draw open tick
+                    opts.color = prevColor;
+                    // draw wick
+                    if (r.wickColor) {
+                        opts.color = r.wickColor;
+                    }
+                    // draw horizontal line of upper and lower bound
+                    r.shapeRenderer.draw(ctx, [[x, yu], [x, yl]], opts);
+                    // draw vertical line of upper and lower bound
+                    r.shapeRenderer.draw(ctx, [[xu, y], [xl, y]], opts);
+
+                    opts.color  = prevColor;
+                    if (r.openColor) {
+                        opts.color = r.openColor;
+                    }
+                    if (yu != null)
+                      // draw upper bound horizontal line
+                      r.shapeRenderer.draw(ctx, [[x+r._tickLength/2, yu], [x-r._tickLength/2, yu]], opts);
+                    if (xu != null)
+                      // draw upper bound vertical line
+                      r.shapeRenderer.draw(ctx, [[xu, y+r._tickLength/2], [xu, y-r._tickLength/2]], opts);
+                    
+                    opts.color  = prevColor;
+                    if (r.closeColor) {
+                        opts.color = r.closeColor;
+                    }
+                    if (yl != null)
+                      // draw lower bound horizontal line
+                      r.shapeRenderer.draw(ctx, [[x+r._tickLength/2, yl], [x-r._tickLength/2, yl]], opts);
+                    if (xl != null)
+                      // draw lower bound vertical line
+                      r.shapeRenderer.draw(ctx, [[xl, y+r._tickLength/2], [xl, y-r._tickLength/2]], opts);
+                    
+                    
+                    opts.color  = prevColor;
+                    
+                    // draw the mean
+                    opts.fillRect = true;
+                    r.shapeRenderer.draw(ctx, [Math.round(x-(r._tickLength/4)), Math.round(y-(r._tickLength/4)), r._tickLength/2, r._tickLength/2], opts);
+
+                    opts.color = prevColor;
+                    opts.fillRect = false;
                 }
             }
             
-            for (var i=xminidx; i<xmaxidx; i++) {
-                x = xp(d[i][0]);
-                y = yp(d[i][1]);
-
-                xu = xp(d[i][2].xupper);
-                xl = xp(d[i][2].xlower);
-                yu = yp(d[i][2].yupper);
-                yl = yp(d[i][2].ylower);
-                close = yp(d[i][1]);
-                //console.log('[',i,']: (x:',d[i][2].xlower,',',d[i][0],',',d[i][2].xupper,'; y:',d[i][2].ylower,',',d[i][1],',',d[i][2].yupper,')');
-
-                o = {};
-                
-                prevColor = opts.color;
-                if (r.openColor) {
-                    opts.color = r.openColor;
-                }
-                // draw open tick
-                opts.color = prevColor;
-                // draw wick
-                if (r.wickColor) {
-                    opts.color = r.wickColor;
-                }
-                // draw horizontal line of upper and lower bound
-                r.shapeRenderer.draw(ctx, [[x, yu], [x, yl]], opts);
-                // draw vertical line of upper and lower bound
-                r.shapeRenderer.draw(ctx, [[xu, y], [xl, y]], opts);
-
-                opts.color  = prevColor;
-                if (r.openColor) {
-                    opts.color = r.openColor;
-                }
-                if (d[i][2].yerr[1])
-                  // draw upper bound horizontal line
-                  r.shapeRenderer.draw(ctx, [[x+r._tickLength/2, yu], [x-r._tickLength/2, yu]], opts);
-                if (d[i][2].xerr[1])
-                  // draw upper bound vertical line
-                  r.shapeRenderer.draw(ctx, [[xu, y+r._tickLength/2], [xu, y-r._tickLength/2]], opts);
-                
-                opts.color  = prevColor;
-                if (r.closeColor) {
-                    opts.color = r.closeColor;
-                }
-                if (d[i][2].yerr[0])
-                  // draw lower bound horizontal line
-                  r.shapeRenderer.draw(ctx, [[x+r._tickLength/2, yl], [x-r._tickLength/2, yl]], opts);
-                if (d[i][2].xerr[0])
-                  // draw lower bound vertical line
-                  r.shapeRenderer.draw(ctx, [[xl, y+r._tickLength/2], [xl, y-r._tickLength/2]], opts);
-                
-                
-                opts.color  = prevColor;
-                
-                // draw the mean
-                opts.fillRect = true;
-                r.shapeRenderer.draw(ctx, [Math.round(x-(r._tickLength/4)), Math.round(y-(r._tickLength/4)), r._tickLength/2, r._tickLength/2], opts);
-
-                opts.color = prevColor;
-                opts.fillRect = false;
-            }
+            ctx.restore();
         }
-        
-        ctx.restore();
     };  
     
     $.jqplot.errorbarRenderer.prototype.drawShadow = function(ctx, gd, options) {
@@ -303,6 +272,65 @@
         }
     };
     
+    function newResetDataBounds() {
+        // overriding for the axes
+        var db = this._dataBounds;
+        db.min = null;
+        db.max = null;
+        var xlower, xupper, ylower, yupper;
+        for (var i=0; i<this._series.length; i++) {
+            var s = this._series[i];
+            var d = s._plotData;
+            if (s.renderer.errorBar) {
+              // Loop through all points
+              for (var j=0; j<d.length; j++) {
+                // First set xupper, xlower, yupper, ylower
+                // Then adjust axis ranges if necessary
+              
+                var eb = d[j][2];
+                if (eb == null) { return }
+                
+                if (this.name == 'xaxis' || this.name == 'x2axis') {
+                    if (eb.xerr) {
+                      // If symmetric
+                      if (isNumeric(eb.xerr)) {
+                        eb.xerr = [eb.xerr, eb.xerr];
+                      }
+                      if (jQuery.isArray(eb.xerr)) {
+                        eb.xlower = d[j][0] - eb.xerr[0];
+                        eb.xupper = d[j][0] + eb.xerr[1];
+                      }
+                    }
+                    if (eb.xlower < db.min || db.min == null) {
+                        db.min = eb.xlower;
+                    }
+                    if (eb.xupper > db.max || db.max == null) {
+                        db.max = eb.xupper;
+                    }
+                } else { // it's a y-axis
+                    if (eb.yerr) {
+                      // If symmetric
+                      if (isNumeric(eb.yerr)) {
+                        eb.yerr = [eb.yerr, eb.yerr];
+                      }
+                      if (jQuery.isArray(eb.yerr)) {
+                        eb.ylower = d[j][1] - eb.yerr[0];
+                        eb.yupper = d[j][1] + eb.yerr[1];
+                      }
+                    }
+                    if (eb.ylower < db.min || db.min == null) {
+                        db.min = eb.ylower;
+                    }
+                    if (eb.yupper > db.max || db.max == null) {
+                        db.max = eb.yupper;
+                    }
+                }
+              }
+            }
+        }
+    }
+    
     //$.jqplot.preInitHooks.push($.jqplot.errorbarRenderer.checkOptions);
+    //$.jqplot.postInitHooks.push($.jqplot.errorbarRenderer.alterResetScale);
     
 })(jQuery);    
